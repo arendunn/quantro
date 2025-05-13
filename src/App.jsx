a082f3b59fb36d950e47ac12e2c608035d0f41a2
Aren Dunn <arendunn@hotmail.com>
2025-05-14 02:10:59 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Navbar from './components/Navbar.jsx'
+
+const App = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App;
