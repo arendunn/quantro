a0968b719254b79c8f885509e49ee7e401b9385d
Aren Dunn <arendunn@hotmail.com>
2025-05-15 10:33:44 +1000

    Responsive Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import GoalForm from '../components/GoalForm.jsx';
 import logo from '/src/assets/quantro.png';
 
-
 const Navbar = () => {
   const [showForm, setShowForm] = useState(false);
 
@@ -12,9 +11,15 @@ const Navbar = () => {
           <img src={logo} alt="Logo" className="h-25" />
           <button
             onClick={() => setShowForm(true)}
-            class className="flex justify-center items-center h-10 w-40 p-4 bg-[#428ce2] rounded"
+            className="flex justify-center items-center font-bold h-10 w-40 bg-blue-500 hover:bg-blue-600 text-white px-4 py-1 rounded hidden sm:flex"
           >
             + Add Goal
+          </button>
+          <button
+            onClick={() => setShowForm(true)}
+            className="flex justify-center items-center font-bold h-10 sm:w-20 bg-blue-500 hover:bg-blue-600 text-white px-4 py-1 rounded sm:hidden"
+          >
+            +
           </button>
           {showForm && (
             <GoalForm onClose={() => setShowForm(false)} />
